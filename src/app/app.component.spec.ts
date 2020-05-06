acb99b34e4ab13e2f2c9e556bf7a98418a437d6f
BrandonTRN <brtn918@gmail.com>
2020-05-05 22:03:53 -0500

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,30 +2,13 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let app: AppComponent;
+  beforeEach(async(()=>{
+    app = new AppComponent();
   }));
+it('Test suma resultado 4', async(()=>{
+expect(app.add(6,2)).toEqual(4);
+}
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'TestingProject'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('TestingProject');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('TestingProject app is running!');
-  });
+))
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('result') result: any;
   title = 'TestingProject';
+
+  public ngOInit(): void{
+
+  }
+  public add(num1: number, num2:number ): number{
+    return Number(num1)+Number(num2);
+  }
+
+  public printadd(num1: number, num2:number): any{
+this.result.nativeElement.value= this.add(num1,num2);
+  }
 }
