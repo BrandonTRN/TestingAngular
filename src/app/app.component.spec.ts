c66e0ddde5953d9da7adcde52539f22eb7ad4287
EdgarFernandez-141299 <edgarfernavila@gmail.com>
2020-05-13 14:50:25 -0500

    Practica 2 Testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,29 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let app: AppComponent;
-  beforeEach(async(()=>{
-    app = new AppComponent();
-  }));
-it('Test suma resultado 4', async(()=>{
-expect(app.add(6,2)).toEqual(4);
-}
 
-))
-});
+  beforeEach(async(()=>{
+
+    TestBed.configureTestingModule({
+      declarations:[
+        AppComponent
+      ],
+      providers:[]
+    }).compileComponents();
+   
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent ]
+    })
+    .createComponent(AppComponent);
+  }));
+
+  it('La suma debe ser igual a 4',async(()=>{
+    (<HTMLInputElement>document.getElementById('num1')).value = '2';
+    (<HTMLInputElement>document.getElementById('num2')).value = '2';
+    document.getElementById('button').click();
+    expect((<HTMLInputElement>document.getElementById('result')).value).toBe('4');
+
+
+  }));
+
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,18 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   @ViewChild('result') result: any;
-  title = 'TestingProject';
+  title = 'Suma-De-Numeros';
 
   public ngOInit(): void{
 
   }
-  public add(num1: number, num2:number ): number{
+
+  
+  public add(num1: number, num2: number ): number{
     return Number(num1)+Number(num2);
   }
 
   public printadd(num1: number, num2:number): any{
-this.result.nativeElement.value= this.add(num1,num2);
+    this.result.nativeElement.value= this.add(num1,num2);
   }
 }
